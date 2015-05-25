47e2d66df7e0cfae6a62d9c2f15d42622973dfba
Malte Krupa <mkrupa@nafn.de>
2015-05-25 18:06:09 +0200

    Removed leaflet cdn.

--- static/custom.js
+++ static/custom.js
@@ -5,36 +5,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
     maxZoom: 18
   }).addTo(map);
   console.log("DOM fully loaded and parsed. Map attached");
-  var popup = L.popup();
-  
-  function DisplayNewPosition(lat,lng) {     
-      if (typeof marker != 'undefined') {
-          map.removeLayer(marker);  // delete previous marker
-          marker = L.marker([lat,lng]).addTo(map);  // add new marker
-      }
-      else {
-          marker = L.marker([lat,lng]).addTo(map);  // add new marker
-      }
-  }
-  
+
+  realtime = L.realtime({
+    type: 'json'
+  }, {
+    start: false
+  }).addTo(map);
+
   var ws = new WebSocket("ws://localhost:8080/ws");
   
   ws.onmessage = function(e) {
-      if (typeof e.data === "string"){
-          try {
-              var object = JSON.parse(e.data);
-              lat=object.Lat
-              lon=object.Lon
-              console.log("Lat: " + lat +", Lon: "+lon);
-              DisplayNewPosition(lat,lon);
-          }
-          catch(err) {
-              console.log("String message received (but not JSON): ", e.data)
-          }
-      } 
-      else {
-          console.log("Other message received (not string)", e.data);
-      }
+    console.log(e.data);
+    var data = JSON.parse(e.data);
+    realtime.update(data);
+    map.fitBounds(realtime.getBounds(), {maxZoom: 17});
   }; 
+
+
+//   var popup = L.popup();
+//   
+//   function DisplayNewPosition(lat,lng) {     
+//       if (typeof marker != 'undefined') {
+//           map.removeLayer(marker);  // delete previous marker
+//           marker = L.marker([lat,lng]).addTo(map);  // add new marker
+//       }
+//       else {
+//           marker = L.marker([lat,lng]).addTo(map);  // add new marker
+//       }
+//   }
+//   
 
 });
