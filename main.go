a5a8d4375cc5cc19f740a32c404ef5b56f991f2f
Malte Krupa <mkrupa@nafn.de>
2015-05-25 18:06:34 +0200

    Implemented Boid with random movement.

--- main.go
+++ main.go
@@ -2,16 +2,70 @@ package main
 
 import (
 	"html/template"
+	"math"
 	"math/rand"
 	"net/http"
 	"time"
 
+	gj "github.com/kpawlik/geojson"
 	"golang.org/x/net/websocket"
 )
 
-type Location struct {
-	Lat float64
-	Lon float64
+var FIVEMETERS = 0.0000449
+
+type Boid struct {
+	Id      int
+	Lon     gj.CoordType
+	Lat     gj.CoordType
+	history gj.Coordinates
+}
+
+func (b *Boid) MoveNorth() {
+	// Move 5 meter to north
+	latitude := float64(b.Lat)
+	tmpLat := latitude + FIVEMETERS
+	newCoordinates := gj.Coordinate{gj.Coord(tmpLat), b.Lon}
+	b.MoveToCoord(newCoordinates)
+}
+
+func (b *Boid) MoveSouth() {
+	// Move 5 meter to south
+	latitude := float64(b.Lat)
+	tmpLat := latitude - FIVEMETERS
+	newCoordinates := gj.Coordinate{gj.Coord(tmpLat), b.Lon}
+	b.MoveToCoord(newCoordinates)
+}
+
+func (b *Boid) MoveWest() {
+	// Move 5 meter to west
+	longitude := float64(b.Lon)
+	tmpLon := longitude + FIVEMETERS/math.Cos(longitude)
+	newCoordinates := gj.Coordinate{b.Lat, gj.Coord(tmpLon)}
+	b.MoveToCoord(newCoordinates)
+}
+
+func (b *Boid) MoveEast() {
+	// Move 5 meter to east
+	longitude := float64(b.Lon)
+	tmpLon := longitude - FIVEMETERS/math.Cos(longitude)
+	newCoordinates := gj.Coordinate{b.Lat, gj.Coord(tmpLon)}
+	b.MoveToCoord(newCoordinates)
+}
+
+func (b *Boid) MoveToCoord(newPos gj.Coordinate) {
+	// Add current coordinates to history and move to the new ones.
+	b.history = append(b.history, newPos)
+	b.Lat = newPos[0]
+	b.Lon = newPos[1]
+}
+
+func (b Boid) PrepareForFrontend() *gj.Feature {
+	// Create a GeoJSON feature for the frontend
+	props := map[string]interface{}{"id": b.Id}
+	c := gj.Coordinate{b.Lon, b.Lat}
+	p := gj.NewPoint(c)
+	f := gj.NewFeature(p, props, nil)
+	return f
 }
 
 func rLat() float64 {
@@ -27,15 +81,26 @@ func rLon() float64 {
 }
 
 func randomHandler(ws *websocket.Conn) {
-	//loc := Location{`{"lat":"50.07", "lon":"8.14"}`}
 	// 50.119, 8.68211
 	// Left up: 50.175444, 8.525362
 	// Right down: 50.104591, 8.753328
+	b1 := Boid{Lat: 50.11021730200894, Lon: 8.750400943993286}
 	for {
-		randLat := rLat()
-		randLon := rLon()
-		loc := Location{Lat: randLat, Lon: randLon}
-		websocket.JSON.Send(ws, loc)
+		randMove := rand.Intn(4)
+		switch {
+		case randMove == 0:
+			b1.MoveNorth()
+		case randMove == 1:
+			b1.MoveSouth()
+		case randMove == 2:
+			b1.MoveWest()
+		case randMove == 3:
+			b1.MoveEast()
+		}
+		f := b1.PrepareForFrontend()
+
+		websocket.JSON.Send(ws, f)
+
 		time.Sleep(500 * time.Millisecond)
 	}
 }
